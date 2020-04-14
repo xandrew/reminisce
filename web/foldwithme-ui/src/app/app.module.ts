ebbfc7190fa8114b704d8b115131e9f8341562d6
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2020-04-14 10:45:44 +0200

    Adds material

--- web/foldwithme-ui/src/app/app.module.ts
+++ web/foldwithme-ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DrawingCanvasComponent } from './drawing-canvas/drawing-canvas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DrawingCanvasComponent } from './drawing-canvas/drawing-canvas.componen
     DrawingCanvasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
