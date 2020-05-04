cc6a5b7c8bf6c448f96f7db77da946f31fe0eab6
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2020-05-04 10:24:47 +0200

    Fixed deploy to work with assets

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ mkdir prod_ui
 pushd web/foldwithme-ui
 ng build --baseHref=/ui/ --outputHashing=all
 popd
-cp web/foldwithme-ui/dist/foldwithme-ui/* prod_ui
+cp -r web/foldwithme-ui/dist/foldwithme-ui/* prod_ui
 
 gcloud app deploy --project=foldwithme --quiet || true
 
